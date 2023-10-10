b85c1f91a15b8e41b0718687216e49b443d593ac
Yichen_Wu <804541459@qq.com>
2023-10-10 16:12:33 +0800

    impling:kafka

--- config/const/ConstantName.go
+++ config/const/ConstantName.go
@@ -1,8 +1,11 @@
 package config
 
 const (
-	TempVideoLocation = "./temp/"
-	FileAuth          = 0644
+	TempVideoLocation  = "./temp/"
+	FileAuth           = 0644
+	KafkaVideoTopic    = "Topic:publishVideo"
+	KafkaVideoProducer = "KafkaClient:VideoMsgProducerId"
+	KafkaVideoConsumer = "KafkaClient:VideoMsgConsumerId"
 )
 
 const (
